2cfdfcd609ffc097807344d7583332f32fb1ad60
Krummers <101184909+Krummers@users.noreply.github.com>
2023-02-22 21:30:03 +0100

    Added set of filenames
    Will be very useful in the future

--- Distribution_functions.py
+++ Distribution_functions.py
@@ -2,6 +2,28 @@ import os
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
+filenames = {"beginner_course", "farm_course", \
+             "kinoko_course", "factory_course", \
+             "castle_course", "shopping_course", \
+             "boardcross_course", "truck_course", \
+             "senior_course", "water_course", \
+             "treehouse_course", "volcano_course", \
+             "desert_course", "ridgehighway_course", \
+             "koopa_course", "rainbow_course", \
+             "old_peach_gc", "old_falls_ds", \
+             "old_obake_sfc", "old_mario_64", \
+             "old_sherbet_64", "old_heyho_gba", \
+             "old_town_ds", "old_waluigi_gc", \
+             "old_desert_ds", "old_koopa_gba", \
+             "old_donkey_64", "old_mario_gc", \
+             "old_mario_sfc", "old_garden_ds", \
+             "old_donkey_gc", "old_koopa_64", \
+             "block_battle", "venice_battle", \
+             "skate_battle", "casino_battle", \
+             "sand_battle", "old_battle4_sfc", \
+             "old_battle3_gba", "old_matenro_64", \
+             "old_CookieLand_gc", "old_House_ds"}
+
 yn = ["yes", "y", "no", "n"]
 
 def download_data(link, location):

de38a69898af3ad9090b203a5136ee75c06421a8
Krummers <krummers10@gmail.com>
2023-10-08 20:56:07 +0200

    Restructure repository

---
+++ Modules/constants.py
@@ -0,0 +1,30 @@
+settings = ["directory", "downloads", "iso-rename", "overwrite-perm", \
+            "perf-monitor", "pref-language", "pycache", "riivo-suffix"]
+    
+contacts = ["archive", "email", "platforms", "wiiki", "wiimmfi"]
+
+binaries = ["yes", "y", "no", "n"]
+
+regions = {"JAP", "KOR", "PAL", "USA"}
+
+filenames = {"beginner_course", "farm_course", \
+             "kinoko_course", "factory_course", \
+             "castle_course", "shopping_course", \
+             "boardcross_course", "truck_course", \
+             "senior_course", "water_course", \
+             "treehouse_course", "volcano_course", \
+             "desert_course", "ridgehighway_course", \
+             "koopa_course", "rainbow_course", \
+             "old_peach_gc", "old_falls_ds", \
+             "old_obake_sfc", "old_mario_64", \
+             "old_sherbet_64", "old_heyho_gba", \
+             "old_town_ds", "old_waluigi_gc", \
+             "old_desert_ds", "old_koopa_gba", \
+             "old_donkey_64", "old_mario_gc", \
+             "old_mario_sfc", "old_garden_ds", \
+             "old_donkey_gc", "old_koopa_64", \
+             "block_battle", "venice_battle", \
+             "skate_battle", "casino_battle", \
+             "sand_battle", "old_battle4_sfc", \
+             "old_battle3_gba", "old_matenro_64", \
+             "old_cookieland_gc", "old_house_ds"}

39487aaa093c47f2815f6065487ed2b2b1d1c52b
Krummers <101184909+Krummers@users.noreply.github.com>
2026-04-13 11:43:52 +0200

    Finish process.py

--- Krummers-Processor/common.py
+++ Krummers-Processor/common.py
@@ -1,438 +1,89 @@
-import json as js
-import os
-import script_utilities.file as fl
-import script_utilities.functions as ft
-import typing as tp
-
-import folders as fd
-
-import Modules.constants as cs
-import Modules.enumerables as eb
-
-selections = fd.get_folder("Selections")
-generations = fd.get_folder("Generations")
-
-class Track(object):
+class CustomTrackDistributions(object):
     
-    def __init__(self, trackid: int, selection: str,
-                 track_type: eb.TrackType = eb.TrackType.Normal) -> None:
-        if not isinstance(trackid, int):
-            raise TypeError("unsupported type for trackid: "
-                            f"'{type(trackid).__name__}'")
+    class Line(object):
         
-        if not isinstance(selection, str):
-            raise TypeError("unsupported type for selection: "
-                            f"'{type(selection).__name__}'")
-        
-        if not isinstance(track_type, eb.TrackType):
-            raise TypeError("unsupported type for track_type: "
-                            f"'{type(track_type).__name__}'")
-        
-        self.trackid = trackid
-        self.selection = selection
-        self.track_type = track_type
-        
-        self.json = fl.File(os.path.join(selections.path, selection, f"{trackid}.json"))
-        self.wbz = fl.File(os.path.join(selections.path, selection, f"{trackid}.wbz"))
-        self.szs = fl.File(os.path.join(generations.path, selection, f"{trackid}.szs"))
-        
-        self.prefix_enum = self.__init_prefix__()
-    
-    def __init_prefix__(self) -> eb.Prefix:
-        if not bool(self.json):
-            return eb.Prefix.Empty
-        
-        prefix = self.get_prefix()
-        if prefix == "SNES":
-            return eb.Prefix.SNES
-        elif prefix == "N64":
-            return eb.Prefix.N64
-        elif prefix == "GBA":
-            return eb.Prefix.GBA
-        elif prefix == "GCN":
-            return eb.Prefix.GCN
-        elif prefix == "GP":
-            return eb.Prefix.GP
-        elif prefix == "GP2":
-            return eb.Prefix.GP2
-        elif prefix == "DS":
-            return eb.Prefix.DS
-        elif prefix == "Wii":
-            return eb.Prefix.Wii
-        elif prefix == "3DS":
-            return eb.Prefix.DS3
-        elif prefix == "DX":
-            return eb.Prefix.DX
-        elif prefix == "Wii U":
-            return eb.Prefix.WiiU
-        elif prefix == "SW":
-            return eb.Prefix.SW
-        elif prefix == "Tour":
-            return eb.Prefix.Tour
-        elif prefix == "RMX":
-            return eb.Prefix.RMX
-        elif prefix == "SW2":
-            return eb.Prefix.SW2
-        elif len(prefix) > 0:
-            return eb.Prefix.Custom
-        else:
-            return eb.Prefix.Empty
-    
-    def __repr__(self) -> str:
-        """Represents the object in a console."""
-        
-        return f"Track[ID={self.trackid}]"
-    
-    def __str__(self) -> str:
-        """Returns the track ID, type and name."""
-        
-        information = self.get_information()
-        name = information["name"]
-        version = information["version"]
-        prefix = self.get_prefix()
-        
-        string = f"{self.trackid}. {self.track_type.name}"
-        string += f" [{prefix} " if prefix else " ["
-        string += f"{name} {version}]"
-        return string
-    
-    def __lt__(self, other: tp.Self) -> bool:
-        if not isinstance(other, Track):
-            raise TypeError("unsupported operand type(s) for '<': "
-                            f"'{type(self).__name__}' and '{type(other).__name__}'")
-        
-        self_data = self.get_information()
-        other_data = other.get_information()
-        
-        if self.track_type != other.track_type:
-            return self.track_type < other.track_type
-        
-        if self_data["name"] != other_data["name"]:
-            return self_data["name"].lower() < other_data["name"].lower()
-        
-        if self.prefix_enum != other.prefix_enum:
-            return self.prefix_enum < other.prefix_enum
-        
-        if self_data["family"] != other_data["family"]:
-            return self_data["family"] < other_data["family"]
-        
-        if self_data["version"] != other_data["version"]:
-            return self_data["version"] < other_data["version"]
-        
-        return False
-    
-    def __le__(self, other) -> bool:
-        if not isinstance(other, Track):
-            raise TypeError("unsupported operand type(s) for '<=': "
-                            f"'{type(self).__name__}' and '{type(other).__name__}'")
-        
-        return self < other or self == other
-    
-    def __eq__(self, other) -> bool:
-        if not isinstance(other, Track):
-            return False
-        
-        return self.trackid == other.trackid and self.track_type == other.track_type
-    
-    def __ge__(self, other) -> bool:
-        if not isinstance(other, Track):
-            raise TypeError("unsupported operand type(s) for '>=': "
-                            f"'{type(self).__name__}' and '{type(other).__name__}'")
-        
-        return self > other or self == other
-    
-    def __gt__(self, other) -> bool:
-        if not isinstance(other, Track):
-            raise TypeError("unsupported operand type(s) for '>': "
-                            f"'{type(self).__name__}' and '{type(other).__name__}'")
-        
-        return not (self <= other)
-    
-    def __ne__(self, other) -> bool:
-        if not isinstance(other, Track):
-            return True
-        
-        return self.trackid != other.trackid or self.track_type != other.track_type
-    
-    def download_json(self) -> None:
-        """Downloads the JSON of the track."""
-        
-        url = f"https://szslibrary.com/api/api.php?id={self.trackid}"
-        ft.download(url, self.json.path, progress = False,
-                    description = f"{self.trackid}.json")
-        
-        try:
-            self.read_json()
-        except js.JSONDecodeError:
-            print(f"Track ID {self.trackid} is unavailable.")
-            self.json.delete()
-            return
-    
-    def download_wbz(self) -> None:
-        """Downloads the WBZ of the track."""
-        
-        url = f"https://szslibrary.com/scripts/download.php?id={self.trackid}"
-        ft.download(url, self.wbz.path, progress = True,
-                    description = f"{self.trackid}.wbz")
-    
-    def convert_szs(self) -> None:
-        """Converts the WBZ of the track to an SZS."""
-        
-        if not bool(self.wbz):
-            raise FileNotFoundError("The system cannot find the file specified: "
-                                    f"'{self.wbz.path}'")
+        def __init__(self, name, version, ct_id, dis_id):
+            self.name = name
+            if version == None:
+                self.version = None
+            else:
+                self.version = str(version)
+            self.ct_id = ct_id
+            self.dis_id = dis_id
             
-        os.system(f"wszst compress --szs \"{self.wbz.path}\" "
-                  f"--dest=\"{self.szs.path}\"")
+            if name.startswith("A "):
+                self.sort = name[2:].lower()
+            elif name.startswith("The "):
+                self.sort = name[4:].lower()
+            elif name.startswith("s☆Ris"):
+                self.sort = "s{}ris CT Pack".format(chr(ord("a") - 1)).lower()
+            else:
+                self.sort = name.lower()
+        
+        def __str__(self):
+            if self.version == None:
+                return "* {{{{Distrib-ref|{}|{}|{}}}}}".format(self.name, self.ct_id, self.dis_id)
+            else:
+                return "* [[{}]] ({})".format(self.name, self.version)
+        
+        def __repr__(self):
+            r = self.name
+            
+            if self.version != None:
+                r += " " + self.version
+            
+            if self.ct_id != None:
+                r += " (" + str(self.ct_id) + ")"
+            
+            if self.dis_id != None:
+                r += " (" + str(self.dis_id) + ")"
+            
+            return r
+        
+    def __init__(self, text):
+        s = text.find("distribution]]s:\n")
+        if s == -1:
+            self.distributions == None
+            return
+        text = text[s + 17:]
+        s = text.find("\n\n")
+        if s != -1:
+            text = text[:s]
+                
+        if text == "* (none)":
+            self.distributions = None
+            return
+        
+        l = text.split("\n")
+        distributions = []
+        
+        for k in range(len(l)):
+            if l[k][2] == "{":
+                s = l[k].find("|") + 1
+                l[k] = l[k][s:]
+                s = l[k].find("|")
+                name = l[k][:s]
+                l[k] = l[k][s + 1:]
+                s = l[k].find("|")
+                ct_id = l[k][:s]
+                l[k] = l[k][s + 1:]
+                s = l[k].find("}")
+                dis_id = l[k][:s]
+                line = CustomTrackDistributions.Line(name, None, ct_id, dis_id)
+            elif l[k][2] == "[":
+                name = l[k][4:l[k].rfind("]") - 1]
+                version = l[k][l[k].rfind("(") + 1:l[k].rfind(")")]
+                line = CustomTrackDistributions.Line(name, version, None, None)
+            distributions.append(line)
+        
+        self.distributions = distributions
     
-    def read_json(self) -> dict:
-        """Returns all the data found in the JSON of the track."""
-        
-        if not bool(self.json):
-            raise FileNotFoundError("The system cannot find the file specified: "
-                                    f"'{self.json.path}'")
-        
-        with open(self.json.path, "r", encoding = "utf-8") as file:
-            data = js.load(file)
-        
-        return data
+    def __str__(self):
+        l = [str(line) for line in self.distributions]
+        return "\n".join(l)
     
-    def get_information(self) -> dict[str, bool | str]:
-        """Returns part of the data of the track in a dictionary."""
-        
-        if not bool(self.json):
-            raise FileNotFoundError("The system cannot find the file specified: "
-                                    f"'{self.json.path}'")
-        
-        data = self.read_json()
-        information = dict()
-        
-        information["is_track"] = bool(data["track_info"]["track_customtrack"])
-        information["is_nintendo"] = bool(data["track_info"]["track_nintendo"])
-        information["is_texture"] = bool(data["track_info"]["track_texturehack"])
-        information["is_edit"] = bool(data["track_info"]["track_change"])
-        
-        prefix = data["track_info"]["prefix"]
-        information["prefix"] = prefix if prefix else ""
-        information["name"] = data["track_info"]["trackname"]
-        information["author"] = data["track_info"]["track_author"]
-        editor = data["track_info"]["track_editor"]
-        information["editor"] = editor if editor else ""
-        information["family"] = data["track_info"]["track_family"]
-        version = data["track_info"]["track_version"]
-        version_extra = data["track_info"]["track_version_extra"]
-        
-        if version_extra == "":
-            version_extra = None
-        
-        if version_extra is not None:
-            version += f"-{version_extra}"
-        
-        information["version"] = version
-        
-        if data["track_info"]["track_prop"] is None:
-            slot = 8
-        else:
-            slot = int(data["track_info"]["track_prop"])
-        
-        information["slot"] = cs.property_slots[f"{slot:#04x}"]
-        
-        if data["track_info"]["track_music"] is None:
-            music = 117
-        else:
-            music = int(data["track_info"]["track_music"])
-        
-        information["music"] = cs.music_slots[f"{music:#04x}"]
-        
-        information["sha1"] = data["track_info"]["track_sha1"]
-        information["date"] = data["track_info"]["track_created"]
-        speed = data["track_info"]["track_speed"]
-        information["speed"] = float(speed) if speed else 1.0
-        laps = data["track_info"]["track_laps"]
-        information["laps"] = int(laps) if laps else 3
-        
-        return information
+    def __repr__(self):
+        return self.distributions.__repr__()
     
-    def get_identifier(self, colour = False) -> str:
-        """Returns the identifier of the track for the BMG."""
-        
-        match self.track_type.name:
-            case "Normal":
-                return ""
-            case "Wish":
-                return "\\c{blue2}+W\\c{off}" if colour else "+W"
-            case "Texture":
-                return "\\c{green}+T\\c{off}" if colour else "+T"
-            case "Edit":
-                return "\\c{red3}+E\\c{off}" if colour else "+E"
-            case "Nintendo":
-                return "\\c{blue}+N\\c{off}" if colour else "+N"
-    
-    def get_prefix(self, colour = False) -> str:
-        """Returns the prefix of the track for the BMG."""
-        
-        information = self.get_information()
-        prefix = information["prefix"]
-        
-        if prefix is None:
-            return ""
-        
-        if not colour:
-            return prefix
-        
-        for spaced_prefix in ["Spyro 1", "Wii U", "TLoZ ALttP",
-                              "TLoZ OoT", "TLoZ TP", "TLoZ TWW"]:
-            prefix = prefix.replace(spaced_prefix, spaced_prefix.replace(" ", "|"))
-        
-        prefixes = prefix.split()
-        prefixes = [prefix.replace("|", " ") for prefix in prefixes]
-        for x, prefix in enumerate(prefixes):
-            match prefix:
-                case "SNES":
-                    prefixes[x] = "\\c{red4}SNES\\c{off}"
-                case "N64":
-                    prefixes[x] = "\\c{yellow}N64\\c{off}"
-                case "GBA":
-                    prefixes[x] = "\\c{yor4}GBA\\c{off}"
-                case "GCN":
-                    prefixes[x] = "\\c{green}GCN\\c{off}"
-                case "DS":
-                    prefixes[x] = "\\c{blue2}DS\\c{off}"
-                case "Wii":
-                    prefixes[x] = "\\c{blue1}Wii\\c{off}"
-                case "3DS":
-                    prefixes[x] = "\\c{yor2}3DS\\c{off}"
-                case "Wii U" | "SW" | "MK8DX":
-                    prefixes[x] = f"\\c{{yor6}}{prefix}\\c{{off}}"
-                case "Tour" | "RMX":
-                    prefixes[x] = f"\\c{{red1}}{prefix}\\c{{off}}"
-                case "SW2":
-                    prefixes[x] = f"\\c{{yor5}}{prefix}\\c{{off}}"
-                case _:
-                    prefixes[x] = f"\\c{{green}}{prefix}\\c{{off}}"
-        
-        return " ".join(prefixes)
-
-class Distribution(object):
-    
-    def __init__(self, selection: str) -> None:
-        if not isinstance(selection, str):
-            raise TypeError("unsupported type for selection: "
-                            f"'{type(selection).__name__}'")
-        
-        self.name = selection
-        self.pickle = fl.PKL(os.path.join(selections.path, selection, "tracklist.pkl"))
-        self.tracks = []
-        
-        self.load_tracks()
-    
-    def __repr__(self) -> str:
-        """Represents the object in a console."""
-        
-        return f"{self.name}[Tracks={len(self.tracks)}]"
-    
-    def __str__(self) -> str:
-        """Returns the tracklist sorted by track ID."""
-        
-        tracks = sorted(self.tracks, key = lambda track:track.trackid)
-        return "\n".join([str(track) for track in tracks])
-    
-    def load_tracks(self) -> None:
-        """Loads all tracks in the file."""
-        
-        self.tracks = []
-        
-        if (tracklist := self.pickle.get_value()) is not None:
-            for track in tracklist:
-                track = Track(track[0], self.name, track[1])
-                self.tracks.append(track)
-    
-    def save_tracks(self) -> None:
-        """Saves all tracks to the file."""
-        
-        tracks = []
-        
-        for track in sorted(self.tracks, key = lambda track:track.trackid):
-            tracks.append((track.trackid, track.track_type))
-        
-        self.pickle.set_value(tracks)
-    
-    def append(self, track: Track) -> None:
-        """Adds a track to the distribution."""
-        
-        if not isinstance(track, Track):
-            raise TypeError("unsupported type for track: "
-                            f"'{type(track).__name__}'")
-        
-        self.tracks.append(track)
-    
-    def remove(self, track: Track) -> None:
-        """Removes a track from the distribution."""
-        
-        if not isinstance(track, Track):
-            raise TypeError("unsupported type for track: "
-                            f"'{type(track).__name__}'")
-        
-        if track not in self.tracks:
-            raise ValueError("'track' not in tracks")
-        
-        self.tracks.remove(track)
-    
-    def get_track(self, trackid: int) -> Track | None:
-        """Returns the track of a track ID in the distribution."""
-        
-        trackids = [track.trackid for track in self.tracks]
-        
-        if trackid not in trackids:
-            return 
-        
-        return self.tracks[trackids.index(trackid)]
-
-class Slot(object):
-    
-    def __init__(self, cup: int, track: int, arena = False) -> None:
-        if not isinstance(cup, int):
-            raise TypeError("unsupported type for cup: "
-                            f"'{type(cup).__name__}'")
-        
-        if not isinstance(track, int):
-            raise TypeError("unsupported type for track: "
-                            f"'{type(track).__name__}'")
-        
-        if track not in range(1, 5):
-            raise ValueError("value of track must be 1, 2, 3 or 4")
-        
-        self.cup = cup
-        self.track = track
-        self.arena = arena
-    
-    def __repr__(self) -> str:
-        """Represents the object in a console."""
-        
-        return str(self)
-    
-    def __str__(self) -> str:
-        """Returns the cup and track with a dot."""
-        
-        string = f"{self.cup}.{self.track}"
-        return f"A{string}" if self.arena else string
-    
-    def __add__(self, amount: int) -> tp.Self:
-        """Adds an integer to a slot."""
-        
-        if not isinstance(amount, int):
-            raise TypeError("unsupported type for amount: "
-                            f"'{type(amount).__name__}'")
-        
-        if amount < 0:
-            raise ValueError("value of amount must be non-negative")
-        
-        result = self
-        maximum_per_cup = 5 if self.arena else 4
-        for _ in range(amount):
-            result.track += 1
-            if result.track > maximum_per_cup:
-                result.cup += 1
-                result.track = 1
-        
-        return result
+    def sort_by_name(self):
+        self.distributions = sorted(self.distributions, key = lambda l:l.sort)
--- Krummers-Processor/process.py
+++ Krummers-Processor/process.py
@@ -1,13 +1,139 @@
+import json as js
 import os
 import script_utilities.file as fl
 import script_utilities.functions as ft
+import subprocess as sp
 
 import folders as fd
 
-selections = fd.get_folder("Output")
+archive_folder = fd.get_folder("Archive")
+input_folder = fd.get_folder("Input")
+output_folder = fd.get_folder("Output")
+
+def collect_information() -> tuple[str]:
+    name = str(input("Distribution name: "))
+    version = str(input("Distribution version: "))
+    author = str(input("Distribution author(s): "))
+    
+    return name, version, author
+
+def compress_files() -> None:
+    os.chdir(input_folder.path)
+    sp.run(["wszst", "compress", "--szs", "--norm", "*.szs", "-o"])
+    os.chdir(input_folder.folder)
+
+def download_szslibrary(sha1: str, filename: str) -> None:
+    url = f"https://szslibrary.com/api/api.php?sha1={sha1}"
+    json = fl.File(os.path.join(output_folder.path, filename))
+    
+    try:
+        ft.download(url, json.path)
+    except TimeoutError:
+        pass
+
+def get_information(path: str) -> dict[str, bool | str]:
+    try:
+        with open(path, "r", encoding = "utf-8") as file:
+            data = js.load(file)
+    except js.JSONDecodeError:
+        return dict()
+    
+    information = dict()
+    
+    information["is_track"] = bool(data["track_info"]["track_customtrack"])
+    information["is_nintendo"] = bool(data["track_info"]["track_nintendo"])
+    information["is_texture"] = bool(data["track_info"]["track_texturehack"])
+    information["is_edit"] = bool(data["track_info"]["track_change"])
+    
+    information["curid"] = data["track_info"]["track_wiki"]
+    prefix = data["track_info"]["prefix"]
+    information["prefix"] = prefix if prefix else ""
+    information["name"] = data["track_info"]["trackname"]
+    information["author"] = data["track_info"]["track_author"]
+    editor = data["track_info"]["track_editor"]
+    information["editor"] = editor if editor else ""
+    information["family"] = data["track_info"]["track_family"]
+    version = data["track_info"]["track_version"]
+    version_extra = data["track_info"]["track_version_extra"]
+    
+    if version_extra == "":
+        version_extra = None
+    
+    if version_extra is not None:
+        version += f"-{version_extra}"
+    
+    information["version"] = version
+    
+    information["sha1"] = data["track_info"]["track_sha1"]
+    information["date"] = data["track_info"]["track_created"]
+    speed = data["track_info"]["track_speed"]
+    information["speed"] = float(speed) if speed else 1.0
+    laps = data["track_info"]["track_laps"]
+    information["laps"] = int(laps) if laps else 3
+    
+    return information
+
+def create_track_list(mode: str) -> None:
+    tracklist = fl.TXT(os.path.join(output_folder.path, "tracklist.txt"))
+    
+    ordered_files = list(map(lambda x:x[0],
+                             sorted(map(lambda file:(file, int(file[:-4])),
+                                        os.listdir(input_folder.path)),
+                                    key = lambda x:x[1])))
+    
+    for file in ordered_files:
+        file = fl.File(os.path.join(input_folder.path, file))
+        
+        track = int(file.filename)
+        # 4 tracks per cup | Pulsar starts 8 cups ahead | Cup 0 does not exist
+        cup = track // 4 + 8 + 1
+        # 4 tracks per cup | Track 0 does not exist
+        slot = track % 4 + 1
+        # file.rename(f"{cup}.{slot}.szs")
+        result = sp.check_output(["wszst", "sha1", f"Input/{file.filename + file.extension}"])
+        sha1 = result.split()[0].decode()
+        
+        json = fl.File(os.path.join(output_folder.path, f"{track}.json"))
+        download_szslibrary(sha1, json.path)
+        information = get_information(json.path)
+        
+        if not information:
+            print(f"Track with SHA1 {sha1} is unknown.")
+            tracklist.append(sha1)
+            file.move(os.path.join(output_folder.path, file.filename + file.extension))
+            json.delete()
+            continue
+        
+        prefix = information["prefix"]
+        name = information["name"]
+        name = f"{prefix} {name}" if prefix else name
+        author = information["author"]
+        version = information["version"]
+        curid = information["curid"]
+        
+        print(f"Track with SHA1 {sha1} is {name} ({author}) {version}.")
+        tracklist.append(f"{name}|{author}|{version}|{curid}")
+    
+    return tracklist
+
+def move_tracklist(tracklist: fl.File, name: str, version: str) -> None:
+    tracklist.move(os.path.join(archive_folder.path, tracklist.filename + tracklist.extension))
+    tracklist.rename(f"{name} {version}{tracklist.extension}")
+
+def clear_input_output() -> None:
+    pass
 
 def main() -> None:
-    pass
+    name, version, author = collect_information()
+    compress = ft.question("Compress the files?")
+    
+    if compress:
+        compress_files()
+    
+    tracklist = create_track_list("Pulsar") # ct engine mode is currently Pulsar by default
+    move_tracklist(tracklist, name, version)
+    
+    clear_input_output()
 
 if __name__ == "__main__":
     main()
